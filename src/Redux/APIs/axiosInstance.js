016b0b0337cd88fc6947c04119b5baa5218f3001
sejal <sejalbharambe2003@gmail.com>
2025-10-29 11:48:50 +0530

    added deployed backend url

--- src/Redux/APIs/axiosInstance.js
+++ src/Redux/APIs/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const BASE_URL = "http://localhost:8080/";
+// export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL= "https://skart-backend-6.onrender.com/";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
