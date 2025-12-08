0c722b42f8c55fa10639d939109551a578ca1f4a
sejal <sejalbharambe2003@gmail.com>
2025-12-08 11:52:25 +0530

    ..

--- src/Redux/APIs/axiosInstance.js
+++ src/Redux/APIs/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const BASE_URL = "http://localhost:8080/";
+// export const BASE_URL = "http://localhost:8080/";
 // export const BASE_URL= "https://skart-backend-6.onrender.com/";
-// export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
