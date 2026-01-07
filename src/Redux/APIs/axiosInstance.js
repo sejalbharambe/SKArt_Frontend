1dba208c18ca83cfb338f2aaee8b895cddacbd76
sejal <sejalbharambe2003@gmail.com>
2026-01-07 15:22:40 +0530

    deployement

--- src/Redux/APIs/axiosInstance.js
+++ src/Redux/APIs/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // export const BASE_URL = "http://localhost:8080/";
-export const BASE_URL = "http://http://3.6.38.145:8080/";
+export const BASE_URL = "http://3.6.38.145:8080/"; //EC2 public IP
 // export const BASE_URL= "https://skart-backend-6.onrender.com/";
 // export const BASE_URL= "skartbackend-production.up.railway.app/";
 // export const BASE_URL = import.meta.env.VITE_API_URL;
